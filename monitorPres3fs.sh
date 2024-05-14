59d6b0d7b11c53c5f276712cf615cc4dba7a5f13
Lyle Pratt <lylepratt@gmail.com>
2024-05-14 23:52:33 +0000

    No more s3fs

--- monitorPres3fs.sh
+++ monitorPres3fs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 inotifywait -m -e create -e close_write -e moved_to --format '%f' /var/pres3fs | while read FILE
 do
-    echo "file moved /var/pres3fs/$FILE -> /var/s3fs/$FILE"
-    if cp /var/pres3fs/$FILE /var/s3fs/$FILE; then
-        echo "File copied successfully, removing original file /var/pres3fs/$FILE"
+    echo "file moved /var/pres3fs/$FILE -> s3://vidamedia/recordings/$FILE"
+    if aws s3 cp /var/pres3fs/$FILE s3://vidamedia/recordings/$FILE --cache-control no-cache; then
+        echo "File copied successfully to S3, removing original file /var/pres3fs/$FILE"
         rm /var/pres3fs/$FILE
     else
-        echo "Failed to copy file /var/pres3fs/$FILE"
+        echo "Failed to copy file /var/pres3fs/$FILE to S3"
     fi
 done
