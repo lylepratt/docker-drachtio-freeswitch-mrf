c1610fcdc6e71d63cf16471c8136e329535f4496
Lyle Pratt <lylepratt@gmail.com>
2024-05-14 14:22:09 +0000

    fixing s3 moves

--- monitorPres3fs.sh
+++ monitorPres3fs.sh
@@ -2,5 +2,10 @@
 inotifywait -m -e create -e close_write -e moved_to --format '%f' /var/pres3fs | while read FILE
 do
     echo "file moved /var/pres3fs/$FILE -> /var/s3fs/$FILE"
-    cp /var/pres3fs/$FILE /var/s3fs/$FILE
+    if cp /var/pres3fs/$FILE /var/s3fs/$FILE; then
+        echo "File copied successfully, removing original file /var/pres3fs/$FILE"
+        rm /var/pres3fs/$FILE
+    else
+        echo "Failed to copy file /var/pres3fs/$FILE"
+    fi
 done
